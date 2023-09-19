f11f26e75b63148747e9f924739015a8257db2c7
Daniel Cui <solunian@gmail.com>
2023-09-18 22:47:14 -0700

    add sort func, reformat

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,12 +1,16 @@
-export function sortColleges(colleges: College[]) {}
+export function sortColleges(colleges: College[]) {
+  colleges.sort((a, b) => a.rating - b.rating);
+}
 
 export function get_two_random_colleges(colleges: College[]) {
-  let mut_colleges = [...colleges];
+  let mut_colleges = [...colleges]; // clone array in order not to mutate parameter
   const idx1 = Math.floor(Math.random() * mut_colleges.length);
   const first_uni = <College>(<unknown>mut_colleges[idx1]);
   mut_colleges.splice(idx1, 1);
 
-  const second_uni = <College>(<unknown>mut_colleges[Math.floor(Math.random() * mut_colleges.length)]);
+  const second_uni = <College>(
+    (<unknown>mut_colleges[Math.floor(Math.random() * mut_colleges.length)])
+  );
 
   return [first_uni, second_uni]; // tuple destructuring!
 }
