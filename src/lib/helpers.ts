5c7713681cd41429ae5a861ab0ceba280650fc1d
Daniel Cui <solunian@gmail.com>
2023-09-18 23:43:04 -0700

    implement rating comp and db update

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,5 +1,44 @@
-export function sortColleges(colleges: College[]) {
-  colleges.sort((a, b) => a.rating - b.rating);
+// using USCF K-factor -> https://en.wikipedia.org/wiki/Elo_rating_system#Theory
+// "The maximum possible adjustment per game, called the K-factor..."
+function get_k_factor(score: number) {
+  if (score < 2100) {
+    return 32;
+  } else if (score < 2400) {
+    return 24;
+  } else {
+    return 16;
+  }
+}
+
+// comparing with elo system! no draws though
+// returns the change in rating in each respective college
+// prevent race condition with setting rating in mongo?
+export function compare_colleges(uni_a: College, uni_b: College, uni_a_won: boolean) {
+  const [Ra, Rb] = [uni_a.rating, uni_b.rating]; // current ratings
+
+  const Ea = 1.0 / (1 + Math.pow(10, (Rb - Ra) / 400.0)); // expected score for a
+  const Eb = 1.0 / (1 + Math.pow(10, (Ra - Rb) / 400.0)); // expected score for a
+
+  const [Ka, Kb] = [get_k_factor(Ra), get_k_factor(Rb)];
+
+  // actual score -> win = 1, lose = 0
+  const Sa = (uni_a_won) ? 1 : 0;
+  const Sb = (!uni_a_won) ? 1 : 0;
+
+  // new ratings
+  // Ra = Ra + Ka * (Sa - Ea); 
+  // Rb = Rb + Kb * (Sb - Eb);
+
+  // change in ratings
+  const dRa = Ka * (Sa - Ea);
+  const dRb = Kb * (Sb - Eb);
+
+  return [dRa, dRb];
+}
+
+// sorts in-place, but also return reference to array
+export function sort_colleges(colleges: College[]) {
+  return colleges.sort((a, b) => a.rating - b.rating);
 }
 
 export function get_two_random_colleges(colleges: College[]) {
--- src/routes/rankings/+page.svelte
+++ src/routes/rankings/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import { sort_colleges } from "$lib/helpers";
   import type { PageData } from "./$types";
 
   export let data: PageData;
 
   const colleges: College[] = JSON.parse(data.colleges_json_str);
+  sort_colleges(colleges);
 </script>
 
 {#each colleges as college}
